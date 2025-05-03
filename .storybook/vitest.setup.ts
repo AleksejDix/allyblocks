e4919d3cfae21d51e6de8b28a173fa5891f0618d
Aleksej Dix <privat@aleksejdix.com>
2025-05-03 13:56:19 +0200

    remove i18n mocks

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -2,24 +2,6 @@ import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 import { beforeAll } from "vitest";
 import { setProjectAnnotations } from "@storybook/react";
 import * as projectAnnotations from "./preview";
-// import React from "react";
-
-// Mock react-i18next
-// vi.mock("react-i18next", () => ({
-//   useTranslation: () => {
-//     return {
-//       t: (str: string) => str,
-//       i18n: {
-//         changeLanguage: () => new Promise(() => {}),
-//       },
-//     };
-//   },
-//   initReactI18next: {
-//     type: "3rdParty",
-//     init: () => {},
-//   },
-//   I18nextProvider: ({ children }: { children: React.ReactNode }) => children,
-// }));
 
 const annotations = setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
 
