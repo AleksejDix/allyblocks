58f111df8e7d163199c717f676bdb7c19adab6d6
Aleksej Dix <privat@aleksejdix.com>
2025-06-03 10:05:21 +0200

    upgrade

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -39,6 +39,11 @@ const parameters: Preview['parameters'] = {
         },
       ],
     },
+
+    // 'todo' - show a11y violations in the test UI only
+    // 'error' - fail CI on a11y violations
+    // 'off' - skip a11y checks entirely
+    test: 'todo',
   },
 }
 
